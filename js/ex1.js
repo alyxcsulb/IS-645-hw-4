88fd784a62090b2c9692ccd96b8de002eb237513
Alyx Faber <alyx.faber@student.csulb.edu>
2021-02-16 18:19:24 -0800

    troubleshoot attempt

--- js/ex1.js
+++ js/ex1.js
@@ -2,11 +2,11 @@ let num1 = parseInt(prompt('Enter Number 1: '));
 let num2 = parseInt(prompt('Enter Number 2: '));
 let operation = (prompt('Enter a mathematical operation (+,-,*,/): '));
 
-if(operation ==='+'){
-    console.log(`${num1} + ${num2} = ${num1 + num2}`)}
-else(operation ==='-');{
-    console.log(`${num1} - ${num2} = ${num1 - num2}`)}
-else(operation ==='*');{
-    console.log(`${num1} * ${num2} = ${num1 * num2}`)}
-else(operation ==='/');{
+if(operation ==='+') {
+    console.log(`${num1} + ${num2} = ${num1 + num2}`)
+} else if(operation ==='-')
+    console.log(`${num1} - ${num2} = ${num1 - num2}`);{
+} else if(operation ==='*') {
+    console.log(`${num1} * ${num2} = ${num1 * num2}`)
+} else if(operation ==='/') {
     console.log(`${num1} / ${num2} = ${num1 / num2}`)};
